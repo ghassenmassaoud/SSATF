4046c4d793e53e803987f31f9230544f2b8a0ccd
ghassenmassaoud <ghassen.massaoud@esprit.tn>
2025-07-28 11:36:43 +0100

    CI Tests

--- Services/TransactionGenerationAPI/jest.config.js
+++ Services/TransactionGenerationAPI/jest.config.js
@@ -13,6 +13,7 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'cobertura', 'json'],
   testTimeout: 10000,
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
   },
